6dca387f5e1e75e5c7258c8930cdf7d5912361dc
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2025-12-29 07:44:10 +0000

    fix: add link to writing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/sections/hero/HeroSection";
 import { BusinessCard } from "@/components/layout/BusinessCard";
 import { ProjectsSection } from "@/components/sections/projects/ProjectsSection";
 import { CapabilitiesSection } from "@/components/sections/capabilities/CapabilitiesSection";
+import { WritingSection } from "@/components/sections/writing/WritingSection";
 import { ConnectSection } from "@/components/sections/connect/ConnectSection";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <BusinessCard />
       <ProjectsSection />
       <CapabilitiesSection />
+      <WritingSection />
       <ConnectSection />
     </main>
   );
